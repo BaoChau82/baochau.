be98fbd83f4b5571d5b841d9bd93b3a459497ab6
baochau <2054050018chau@ou.edu.vn>
2022-10-15 14:22:06 +0700

    Init Project

---
+++ 2054050018_NguyenVuongBaoChau/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _2054050018_NguyenVuongBaoChau
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
